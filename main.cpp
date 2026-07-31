cd511d326cb9e04a77577c15ae55a0955d96ab4b
Satveerk31 <satveerk2006@gmail.com>
2026-07-31 20:59:51 +0530

    Initial project setup

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "=====================================\n";
+    cout << "     SMART ROUTE PLANNER\n";
+    cout << "=====================================\n";
+    cout << "Project Created Successfully!\n";
+
+    return 0;
+}
